93a607cc4a83607589d253d4fa70c556e59d74a1
tailow <tailows@outlook.com>
2023-07-07 23:31:29 +0300

    Added traits to person scriptable object

--- Assets/_SocialMediaGame/Scripts/PersonSriptableObject.cs
+++ Assets/_SocialMediaGame/Scripts/PersonSriptableObject.cs
@@ -1,11 +1,22 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Person", menuName = "ScriptableObjects/Person")]
 public class PersonSriptableObject : ScriptableObject
 {
+    [System.Serializable]
+    public class Trait
+    {
+        public TraitScriptableObject traitScriptableObject;
+
+        public float drainRate;
+    }
+    
     public string personName;
 
     public Sprite personSprite;
 
     public float globalDrainRate;
+    
+    public List<Trait> traits;
 }
